e45aafb8c36fd61ce4dbf849a50f9a958358d090
GideonRotic1 <rotichgid38@gmail.com>
2024-01-22 16:04:11 +0300

    did objects lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: 'John Doe',
+    streetAddress: '123 Main St',
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    // Create a new object with the updated value
+    return {
+      ...employee,
+      [key]: value,
+    };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    // Create a new object without the specified key
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
